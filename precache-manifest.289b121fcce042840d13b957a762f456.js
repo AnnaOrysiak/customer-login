ead3d93b77477dca498fc1ab64e9e3f5406ec752
AnnaOrysiak <walachan88@gmail.com>
2019-10-03 08:54:40 +0200

    Updates

---
+++ precache-manifest.289b121fcce042840d13b957a762f456.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6faf584ae5d5b2d4161b513c67bdf71",
+    "url": "/customer-login/index.html"
+  },
+  {
+    "revision": "24fba1934cfcadc5795d",
+    "url": "/customer-login/static/css/2.7d3372dc.chunk.css"
+  },
+  {
+    "revision": "8535b963c4836944c1bd",
+    "url": "/customer-login/static/css/main.84c920a9.chunk.css"
+  },
+  {
+    "revision": "24fba1934cfcadc5795d",
+    "url": "/customer-login/static/js/2.9ad53b54.chunk.js"
+  },
+  {
+    "revision": "8535b963c4836944c1bd",
+    "url": "/customer-login/static/js/main.e11ab393.chunk.js"
+  },
+  {
+    "revision": "4907d2d22c6cbed763cf",
+    "url": "/customer-login/static/js/runtime-main.75d62769.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/customer-login/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/customer-login/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/customer-login/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/customer-login/static/media/notification.c0d3c94c.eot"
+  }
+]);
