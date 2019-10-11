a44db8071fba9b539e595599f6a3c8828ea377c5
AnnaOrysiak <walachan88@gmail.com>
2019-10-11 12:05:13 +0200

    refactored fetch in SubmitButton

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,51 +1,50 @@
 import React from 'react';
 import "./SubmitButton.css";
 
+const baseURL = "https://reqres.in/api/login";
+
+const formTestPass = {
+  email: "eve.holt@reqres.in",
+  password: "cityslicka"
+};
+
+const formFetch = async () => {
+  const settings = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formTestPass)
+  };
+
+  try {
+    const fetchResponse = await fetch(`${baseURL}`, settings);
+    const data = await fetchResponse.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+
+}
+
 const SubmitButton = (props) => {
 
   const { formSend, handleStatusForm, login, handleLogin, validated } = props;
-
-  const formTestPass = {
-    email: "eve.holt@reqres.in",
-    password: "cityslicka"
-  };
-
   const handleSubmit = () => {
 
     if (formSend && login) {
-
-      handleStatusForm("info")
-
+      handleStatusForm("info");
     } else {
       if (validated) {
-
-        fetch("https://reqres.in/api/login", {
-          method: "post",
-          headers: {
-            "Content-type": "application/json; charset=UTF-8"
-          },
-          body: JSON.stringify(formTestPass)
-        })
-          .then(res => {
-            if (res.ok) {
-              return res.json();
-            }
-            throw Error("Ooops... Something went wrong.")
-          }
-          )
+        formFetch()
           .then(data => {
-
             handleStatusForm("success");
 
             // Console log left on purpose
             // Preview of receiving data
-            console.log(data);
+            console.log(`token: ${data.token}`);
             handleLogin();
-
-          })
-          .catch(err => {
-            handleStatusForm("error");
-            return err;
           })
       }
     }
